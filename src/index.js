2813065a428bb5764d741bee20af6b9aabeeffd0
Pama Lee <pama@pamalee.cn>
2024-11-11 16:40:34 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,45 +1,23 @@
+import { HelloHelper } from './utils/helper.js';
+import { HelloUI } from './components/ui.js';
+
 class HelloWorldPlugin {
   constructor() {
     this.id = 'moodesk-hello-world';
     this.name = 'Hello World Plugin';
     this.version = '1.0.0';
-    this.description = 'A simple hello world plugin for Moodesk';
-    this.author = 'Your Name';
-
-    // 定义钩子
+    this.helper = new HelloHelper();
+    this.ui = new HelloUI();
+    
     this.hooks = {
-      'onInit': [this.sayHello.bind(this)]
+      'onInit': [this.initialize.bind(this)]
     };
-
-    // 定义权限
-    this.permissions = [];
   }
 
-  async onInstall() {
-    console.log('Hello World Plugin installed');
+  async initialize() {
+    await this.helper.setup();
+    await this.ui.render();
   }
-
-  async onEnable() {
-    console.log('Hello World Plugin enabled');
-  }
-
-  async onDisable() {
-    console.log('Hello World Plugin disabled');
-  }
-
-  async onUninstall() {
-    console.log('Hello World Plugin uninstalled');
-  }
-
-  async sayHello() {
-    alert('Hello World from Moodesk Plugin!');
-  }
-}
-
-// 自动注册插件
-if (typeof window !== 'undefined' && window.moodesk) {
-  const helloWorldPlugin = new HelloWorldPlugin();
-  window.moodesk.pluginManager.registerPlugin(helloWorldPlugin);
 }
 
 export default HelloWorldPlugin;
